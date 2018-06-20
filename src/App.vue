5aa7c0b3c8381568b2233d1a52099dbcc355ca90
felicia mtoka <feliciahmtoka@gmail.com>
2018-06-20 15:59:26 +0300

    added a todolist

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
 <template>
   <div class="container">
     <h3>{{ title }}</h3>
+    <form>
+    <textarea placeholder="Add new Task" v-model="newTask"></textarea>
+    <p>{{ newTask }}</p>
+    <button type="button" v-on:click="addTask">Add Task {{tasks.length}}</button>
+    </form>
+    <ul>
+    <li v-for="task in tasks">{{ task }}</li>
+    </ul>
   </div>
 </template>
 
@@ -8,8 +16,20 @@
 export default {
   data: function() {
     return {
-      title: 'My App'
+      title: 'My Todo List',
+      newTask:'This is a sample',
+      tasks:[
+
+      ]
     }
+  },
+  methods:{
+  	addTask(){
+  		
+  		this.tasks.unshift(this.newTask)
+  		this.newTask = '';
+
+  	}
   }
 }
 </script>
